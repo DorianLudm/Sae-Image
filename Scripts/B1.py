740748dae2a444d80b569c4189f7d5c740da67e6
DORIAN LUDMANN <dorianludm7@gmail.com>
2023-01-12 10:32:09 +0100

    B1 glitches

--- Scripts/B1.py
+++ Scripts/B1.py
@@ -1,8 +1,8 @@
 from PIL import Image
 
-i = Image.open("./Images/Image3.bmp")
+i = Image.open("./Images/Imagetest.bmp")
 for y in range(i.size[1]): #Colonne
     for x in range(i.size[0]): #Ligne
         c = i.getpixel((x,y))
-        i.putpixel((y,x),(c))
-i.save("Imageout0.bmp")
+        i.putpixel((x,y),(c))
+i.save("./Images/Imageout0.bmp")
