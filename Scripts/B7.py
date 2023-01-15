e3474f7f6f577c7802a54958987497aef37b859a
DORIAN LUDMANN <dorianludm7@gmail.com>
2023-01-15 16:54:51 +0100

    B7 Modif

--- Scripts/B7.py
+++ Scripts/B7.py
@@ -6,7 +6,7 @@ lettre = {"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10,
           "v":22, "w":23, "x":24, "y":25, "z":26}
 liste_val = []
 texte_encoded = ""
-if len(texte_to_encode) == len(cle_encodage):
+if len(texte_to_encode) <= len(cle_encodage):
     for i in range(len(texte_to_encode)):
         liste_val.append(((lettre[texte_to_encode[i]]+lettre[cle_encodage[i]])%26))
     for val in liste_val:
@@ -15,13 +15,13 @@ if len(texte_to_encode) == len(cle_encodage):
                 texte_encoded = texte_encoded + str(char)
     print(texte_encoded)
 else:
-    print("La clé doit être de la même taille que le message à encoder")
+    print("La clé doit être au moins de la même taille que le message à encoder")
     
 ###Partie DECODAGE###
 texte_encoded = "cqqntayrncfuficjppmybakrhl"
 liste_val = []
 texte_decoded = ""
-if len(texte_encoded) == len(cle_encodage):
+if len(texte_encoded) <= len(cle_encodage):
     for i in range(len(texte_encoded)):
         liste_val.append(((lettre[texte_encoded[i]]-lettre[cle_encodage[i]]+26)%26))
     print(liste_val)
@@ -31,4 +31,4 @@ if len(texte_encoded) == len(cle_encodage):
                 texte_decoded = texte_decoded + str(char)
     print(texte_decoded)
 else:
-    print("La clé doit être de la même taille que le message puisse être décodé")
+    print("La clé doit être au moins de la même taille que le message puisse être décodé")
